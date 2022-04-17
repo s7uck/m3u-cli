7d094cc1e445b9c9708e9a0b2c92c9fe0f74a2bc
StuckDuck <stuck@stuckduck.it>
2022-04-17 12:00:37 +0200

    Fix issue with relative paths

--- jiggl3y.ps1
+++ jiggl3y.ps1
@@ -5,16 +5,12 @@ if (-not $playlist) {
 }
 $playlist = $playlist.Replace('.m3u', '') + '.m3u'
 
-$files = @(Get-ChildItem)
+$files = @((Resolve-Path (Get-ChildItem).Name).Path)
 
 function Generate-Playlist($folder = $folder, $files = $files, $name = $playlist) {
-	if (-not [System.IO.Path]::IsPathRooted($name)) {
-		$name = Join-Path -Path $folder -ChildPath $name
-	}
 	if (Test-Path $name -PathType Leaf) { Remove-Item $name }
 
-	$files | ForEach-Object { $_.Name } | 
-	Sort-Object | Out-File -FilePath "$folder\$name" -Encoding UTF8
+	$files | Out-File -FilePath "$folder\$name" -Encoding UTF8
 }
 
 Generate-Playlist
