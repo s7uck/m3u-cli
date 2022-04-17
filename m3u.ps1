39f4782912227486d298b948974bba88519785a3
StuckDuck <stuck@stuckduck.it>
2022-04-17 16:03:28 +0200

    Fix conflict when using files from other folders

--- m3u.ps1
+++ m3u.ps1
@@ -5,6 +5,7 @@ if (-not $playlist) {
 }
 $playlist = $playlist.Replace('.m3u', '') + '.m3u'
 
+$folder = Resolve-Path $folder
 $files = @((Resolve-Path (Get-ChildItem).Name).Path)
 
 function Generate-Playlist($folder = $folder, $files = $files, $name = $playlist) {
@@ -14,3 +15,4 @@ function Generate-Playlist($folder = $folder, $files = $files, $name = $playlist
 }
 
 Generate-Playlist
+Write-Host "Created playlist $playlist with $($files.Count) files from $folder" -ForegroundColor Blue
