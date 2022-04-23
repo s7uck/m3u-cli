48d0cb9a916d35112646c300dc5d5e1088e467a8
StuckDuck <stuck@stuckduck.it>
2022-04-23 19:28:29 +0200

    Now only including media files

--- m3u.ps1
+++ m3u.ps1
@@ -6,11 +6,12 @@ if (-not $playlist) {
 $playlist = $playlist.Replace('.m3u', '') + '.m3u'
 
 $folder = Resolve-Path $folder
-$files = @((Resolve-Path (Get-ChildItem).Name).Path)
+$files = @((
+	Resolve-Path (
+		Get-ChildItem .\* -Include "*.mp4", "*.mp3", "*.mkv", "*.avi", "*.ogg", "*.mpg", "*.mpeg", "*.wav", "*.wave", "*.opus"
+	).Name).Path)
 
 function Generate-Playlist($folder = $folder, $files = $files, $name = $playlist) {
-	if (Test-Path $name -PathType Leaf) { Remove-Item $name }
-
 	$files | Out-File -FilePath "$folder\$name" -Encoding UTF8
 }
 
